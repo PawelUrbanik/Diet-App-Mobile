203ea8ef43251f648dc3c195468d12bca5fee8ec
Pawel <pawel.urbanik.dev@gmail.com>
2026-06-21 13:15:12 +0200

    plan: add new predefined week template and seed spinach into products list

--- app/src/main/java/pl/pawel/diet_app_mobile/data/repository/PredefinedWeekTemplates.kt
+++ app/src/main/java/pl/pawel/diet_app_mobile/data/repository/PredefinedWeekTemplates.kt
@@ -81,5 +81,57 @@ internal object PredefinedWeekTemplates {
                 PredefinedSlotConfig(SUN, DINNER, "CHLEB ŻYTNI RAZOWY"),
             ),
         ),
+        PredefinedTemplateConfig(
+            id = "predef:kk-miesiac-1-1",
+            name = "Dieta KK - Miesiąc 1.1",
+            rawSlots = listOf(
+                // Poniedziałek
+                PredefinedSlotConfig(MON, BREAKFAST, "OWSIANKA Z ODŻYWKĄ BIAŁKOWĄ (BANAN I ORZECHY)"),
+                PredefinedSlotConfig(MON, SECOND_BREAKFAST, "KANAPKI Z POLĘDWICĄ SOPOCKĄ"),
+                PredefinedSlotConfig(MON, LUNCH, "KOKTAJL BANAN, KAKAO, MLEKO"),
+                PredefinedSlotConfig(MON, LUNCH, "MAKARON Z PESTO I KURCZAKIEM"),
+                PredefinedSlotConfig(MON, SNACK, "SEREK WIEJSKI LIGHT Z WARZYWAMI I PIECZYWEM"),
+                PredefinedSlotConfig(MON, DINNER, "KANAPKI Z TWAROŻKIEM SZCZYPIOREK I RZODKIEWKA"),
+                // Wtorek
+                PredefinedSlotConfig(TUE, BREAKFAST, "JAJECZNICA Z POMIDORAMI I CHLEBEM ŻYTNIM"),
+                PredefinedSlotConfig(TUE, SECOND_BREAKFAST, "SEREK WIEJSKI Z POMIDOREM I CHLEBEM ŻYTNIM"),
+                PredefinedSlotConfig(TUE, LUNCH, "SPAGHETTI Z MIĘSEM MIELONYM Z SZYNKI"),
+                PredefinedSlotConfig(TUE, SNACK, "TWARÓG Z BANANEM"),
+                PredefinedSlotConfig(TUE, DINNER, "OWSIANKA Z ODŻYWKĄ BIAŁKOWĄ (BANAN)"),
+                // Środa
+                PredefinedSlotConfig(WED, BREAKFAST, "KASZA JAGLANA Z BANANEM, KAKAO I ŻURAWINĄ"),
+                PredefinedSlotConfig(WED, SECOND_BREAKFAST, "TORTILLA Z SZYNKĄ, SEREM ŻÓŁTYM I SAŁATĄ LODOWĄ"),
+                PredefinedSlotConfig(WED, LUNCH, "SPAGHETTI Z MIĘSEM MIELONYM Z SZYNKI"),
+                PredefinedSlotConfig(WED, SNACK, "SKYR Z JABŁKIEM"),
+                PredefinedSlotConfig(WED, DINNER, "SYRNIKI Z DŻEMEM TRUSKAWKOWYM"),
+                // Czwartek
+                PredefinedSlotConfig(THU, BREAKFAST, "TOSTY Z MOZZARELLĄ I SZYNKĄ Z PIERSI KURCZAKA"),
+                PredefinedSlotConfig(THU, SECOND_BREAKFAST, "TORTILLA Z ŁOSOSIEM, SERKIEM I SAŁATĄ LODOWĄ", servings = 2.0),
+                PredefinedSlotConfig(THU, LUNCH, "KURCZAK Z MOZZARELLĄ, BAZYLIĄ I POMIDOREM"),
+                PredefinedSlotConfig(THU, SNACK, "RYŻ Z ODŻYWKĄ BIAŁKOWĄ I MROŻONYMI TRUSKAWKAMI"),
+                PredefinedSlotConfig(THU, DINNER, "OWSIANKA Z ODŻYWKĄ BIAŁKOWĄ (MALINY)"),
+                // Piątek
+                PredefinedSlotConfig(FRI, BREAKFAST, "PASTA Z MAKRELI, JAJKA I KWASZONEGO OGÓRKA"),
+                PredefinedSlotConfig(FRI, BREAKFAST, "CHLEB ŻYTNI RAZOWY"),
+                PredefinedSlotConfig(FRI, SECOND_BREAKFAST, "KOKTAJL KEFIR Z MALINAMI I BANANEM"),
+                PredefinedSlotConfig(FRI, LUNCH, "KURCZAK Z MOZZARELLĄ, BAZYLIĄ I POMIDOREM"),
+                PredefinedSlotConfig(FRI, SNACK, "SEREK WIEJSKI Z POMIDOREM I CHLEBEM ŻYTNIM"),
+                PredefinedSlotConfig(FRI, DINNER, "TWAROŻEK CHUDY Z RZODKIEWKĄ I GRZANKAMI"),
+                // Sobota
+                PredefinedSlotConfig(SAT, BREAKFAST, "RACUCHY JABŁKOWE Z CYNAMONEM NA MLEKU"),
+                PredefinedSlotConfig(SAT, SECOND_BREAKFAST, "PASTA Z MAKRELI, JAJKA I KWASZONEGO OGÓRKA"),
+                PredefinedSlotConfig(SAT, SECOND_BREAKFAST, "CHLEB ŻYTNI RAZOWY"),
+                PredefinedSlotConfig(SAT, LUNCH, "SCHAB DUSZONY ZE SZPINAKIEM W SOSIE MIODOWO-MUSZTARDOWYM"),
+                PredefinedSlotConfig(SAT, SNACK, "TOSTY Z MOZZARELLĄ, WARZYWAMI I POLĘDWICĄ"),
+                PredefinedSlotConfig(SAT, DINNER, "TORTILLA Z SZYNKĄ, SEREM ŻÓŁTYM I SAŁATĄ LODOWĄ"),
+                // Niedziela
+                PredefinedSlotConfig(SUN, BREAKFAST, "OWSIANKA Z ODŻYWKĄ BIAŁKOWĄ (MOCNA ŻURAWINA)"),
+                PredefinedSlotConfig(SUN, SECOND_BREAKFAST, "TORTILLA Z SZYNKĄ, SEREM ŻÓŁTYM I SAŁATĄ LODOWĄ"),
+                PredefinedSlotConfig(SUN, LUNCH, "SCHAB DUSZONY ZE SZPINAKIEM W SOSIE MIODOWO-MUSZTARDOWYM"),
+                PredefinedSlotConfig(SUN, SNACK, "TOSTY"),
+                PredefinedSlotConfig(SUN, DINNER, "PASTA Z MAKRELI, JAJKA I KWASZONEGO OGÓRKA"),
+                PredefinedSlotConfig(SUN, DINNER, "CHLEB ŻYTNI RAZOWY"),
+            ),
+        ),
     )
 }
